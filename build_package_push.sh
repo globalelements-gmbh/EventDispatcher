44d06cdd816a8fe751bdcd3e1d8276d799eb4e40
Christian Klauenboesch <cklauenboesch@globalelements.ch>
2021-03-01 19:15:08 +0100

    provide release version through env variable

--- build_package_push.sh
+++ build_package_push.sh
@@ -3,11 +3,7 @@
 # do not honor failure - abort early
 set -e
 
-# Restore, build, test
-dotnet restore
-dotnet build
-dotnet test ./*Test*/*.csproj
-
+echo "-- Verify config"
 # Verify the presence of the nuget api key
 if [[ -z "${NUGET_API_KEY}" ]]; then
   echo "Set NUGET_API_KEY environment variable for the api key used to publish to the Nuget gallery"
@@ -18,9 +14,17 @@ if [[ -z "${NUGET_VERSION}" ]]; then
   exit 1
 fi
 
+# Restore, build, test
+echo "-- Prepare"
+dotnet restore
+dotnet build
+dotnet test ./*Test*/*.csproj
+
 # Build, package and push the NuGet
+echo "-- Pack and publish for version: ${NUGET_VERSION}"
 dotnet clean
-rm -f ./GlobalElements.EventDispatcher/bin/Release/GlobalElements.EventDispatcher*.nupkg
+rm -f ./GlobalElements.EventDispatcher/bin/R elease/GlobalElements.EventDispatcher*.nupkg
+sed -i "s/VERSIONSTRING/${NUGET_VERSION}/g" ./GlobalElements.EventDispatcher/EventDispatcher.nuspec
 dotnet build -c Release
-dotnet pack ./GlobalElements.EventDispatcher/GlobalElements.EventDispatcher.csproj -c Release -p:NuspecFile=EventDispatcher.nuspec;version=$NUGET_VERSION
+dotnet pack ./GlobalElements.EventDispatcher/GlobalElements.EventDispatcher.csproj -c Release -p:NuspecFile=EventDispatcher.nuspec
 dotnet nuget push ./GlobalElements.EventDispatcher/bin/Release/GlobalElements.EventDispatcher*.nupkg -s https://www.nuget.org -k ${NUGET_API_KEY}
